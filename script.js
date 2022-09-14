6168fd2245ed4d2dee345341839b204ff30c8fbb
pugaljp <103755873+pugaljp@users.noreply.github.com>
2022-09-14 11:35:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+
+let countdown=10;  
+setTimeout(() => {
+    document. querySelector(".countdown").innerText = --countdown;
+    setTimeout(() => {
+        document. querySelector(".countdown").innerText = --countdown;
+        setTimeout(() => {
+            document. querySelector(".countdown").innerText = --countdown;
+            setTimeout(() => {
+                document. querySelector(".countdown").innerText = --countdown;
+                setTimeout(() => {
+                    document. querySelector(".countdown").innerText = --countdown;
+                    setTimeout(() => {
+                        document. querySelector(".countdown").innerText = --countdown;
+                        setTimeout(() => {
+                            document. querySelector(".countdown").innerText = --countdown;
+                            setTimeout(() => {
+                                document. querySelector(".countdown").innerText = --countdown;
+                                setTimeout(() => {
+                                    document. querySelector(".countdown").innerText = --countdown;
+                                    setTimeout(() => {
+                                        document. querySelector(".countdown").innerText = "HAPPY INDEPENDENCE DAY ";
+                                     },1000);                                
+                                 },1000);                            
+                             },1000);                        
+                         },1000);                    
+                     },1000);
+                },1000);
+            },1000);
+        },1000);
+     },1000);
+},1000);
+
